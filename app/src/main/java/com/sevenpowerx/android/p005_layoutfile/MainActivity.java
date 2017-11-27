7d8434421c0dc8000cace7364a5f07c93858d3c2
splaa <splaandrey@gmail.com>
2017-11-27 18:30:10 +0100

    import app’s main activity
    2. Start the SDK
    
    Open your app’s main activity class and add the following import statements.
    
    import com.microsoft.appcenter.AppCenter;
    import com.microsoft.appcenter.analytics.Analytics;
    import com.microsoft.appcenter.crashes.Crashes;

--- app/src/main/java/com/sevenpowerx/android/p005_layoutfile/MainActivity.java
+++ app/src/main/java/com/sevenpowerx/android/p005_layoutfile/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sevenpowerx.android.p005_layoutfile;
-
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
